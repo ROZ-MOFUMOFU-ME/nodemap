ce7eb48b2f50a0bd33d04b55ce2ee59baf8758d2
Aoi Emerauda <Aaoi@emerauda.com>
2024-11-07 19:04:54 +0900

    Add mockData for testing and API tests

---
+++ test/mockData.js
@@ -0,0 +1,27 @@
+module.exports = {
+    peerInfo: [
+        {
+            addr: '127.0.0.1',
+            subver: '/Satoshi:0.19.0.1/',
+            version: '70015',
+            startingheight: 123456
+        }
+    ],
+    networkInfo: {
+        subversion: '/Satoshi:0.19.0.1/',
+        protocolversion: '70015',
+        localaddresses: [
+            { address: '127.0.0.1' }
+        ]
+    },
+    miningInfo: {
+        blocks: 123456
+    },
+    geoLocation: {
+        loc: '35.6895,139.6917',
+        country: 'JP',
+        city: 'Tokyo',
+        org: 'Test Org'
+    },
+    dnsLookup: 'test-dns'
+};
---
+++ test/test.js
@@ -0,0 +1,47 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, setClient } = require('../app');
+const mockData = require('./mockData');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const mockClient = {
+    command: async (cmd) => {
+        if (cmd === 'getpeerinfo') {
+            return mockData.peerInfo;
+        } else if (cmd === 'getnetworkinfo') {
+            return mockData.networkInfo;
+        } else if (cmd === 'getmininginfo') {
+            return mockData.miningInfo;
+        }
+    }
+};
+
+setClient(mockClient);
+
+describe('API tests', function() {
+    this.timeout(10000);
+
+    it('should respond with HTTP status 200', function(done) {
+        chai.request(app)
+            .get('/peer-locations')
+            .end((err, res) => {
+                if (err) {
+                    console.error('Test failed with error:', err);
+                    return done(err);
+                } else {
+                    console.log('Response status:', res.status);
+                    console.log('Response body:', res.body);
+                    expect(res).to.have.status(200);
+                    done();
+                }
+            });
+    });
+});
+
+after((done) => {
+    console.log('All tests done, exiting process.');
+    done();
+    process.exit();
+});
